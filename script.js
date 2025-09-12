88e02f8cd6437379b2977ad530162bafc33bbb11
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-09-12 16:47:42 +0530

    mapty-1

--- script.js
+++ script.js
@@ -17,16 +17,17 @@ if (navigator.geolocation) {
       const { longitude } = position.coords;
       console.log(position);
       console.log(latitude, longitude);
+      const coords = [latitude, longitude];
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-      const map = L.map("map").setView([51.505, -0.09], 13);
+      const map = L.map("map").setView(coords, 13); //to zoom in and out change 13
 
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([51.5, -0.09])
+      L.marker(coords)
         .addTo(map)
         .bindPopup("A pretty CSS popup.<br> Easily customizable.")
         .openPopup();
@@ -37,4 +38,4 @@ if (navigator.geolocation) {
   );
 }
 
-console.log(firstName);
+// console.log(firstName);
